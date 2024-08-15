ec425e7184e64112f4e3bdf75dfbab0c6222f918
daviizinhow <ferreiradesouzadavi4@gmail.com>
2024-08-15 15:17:46 -0300

    primeira commit

---
+++ funcoes.c
@@ -0,0 +1,192 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <ctype.h>
+#include <math.h>
+
+const char* separastring(const char* str, int inicio, int fim)
+{
+
+    int comprimento_substring = fim - inicio + 1;
+    
+    static char resultado[1000] = {0};
+
+    strncpy(resultado, str + inicio, comprimento_substring);
+    resultado[comprimento_substring] = '\0'; 
+    
+    return resultado;
+}
+
+
+const char* centena(char *cent)
+{
+    static char centena_extenso[1000] = {0};
+    
+    char unidades[15][100] = {"um", "dois", "tres", "quatro", "cinco", "seis", "sete", "oito", "nove"};
+    char especiais[15][100] = {"dez", "onze", "doze", "treze", "quatorze", "quinze", "dezesseis", "dezessete", "dezoito", "dezenove", "cem"};
+    char dezenas[15][100] = {"vinte", "trinta", "quarenta", "cinquenta", "sessenta", "setenta", "oitenta", "noventa"};
+    char centenas[15][100] = {"cento", "duzentos", "trezentos", "quatrocentos", "quinhentos", "seiscentos", "setecentos", "oitocentos", "novecentos"};
+
+    int var1, var2,var3;
+
+    
+    var1 = cent[0] - '0';
+    var2 = cent[1] - '0';
+    var3 = cent[2] - '0';
+
+    strcpy(centena_extenso, "");
+
+
+    if(var1 == 0)
+    {
+        if(var2 == 0)
+        {
+            if(var3 == 0)
+            {
+                strcpy(centena_extenso, "l");
+                return centena_extenso;
+            }
+            if(var3 > 0)
+            {
+                strcpy(centena_extenso, unidades[var3-1]);
+            }
+
+        }
+        else if(var2 == 1)
+        {
+            strcpy(centena_extenso, especiais[var3]);
+        }
+        else
+        {
+            strcat(centena_extenso, dezenas[var2-2]);
+            if(var3 > 0)
+            {
+                strcat(centena_extenso, " e ");
+                strcat(centena_extenso, unidades[var3-1]);
+            }
+            
+
+        }
+    }
+
+    else if(var1 == 1 && var2 == 0 && var3 == 0)
+    {
+        strcpy(centena_extenso, especiais[10]);
+    }
+
+
+    else
+    {
+        strcat(centena_extenso, centenas[var1-1]);
+        strcat(centena_extenso, " e ");
+        if(var2 == 1)
+        {
+            strcat(centena_extenso, especiais[var3]);
+        }
+        else
+        {
+            strcat(centena_extenso, dezenas[var2-2]);
+            if(var3 > 0)
+            {
+                strcat(centena_extenso, " e ");
+                strcat(centena_extenso, unidades[var3-1]);
+            }
+            
+
+        }
+    }
+
+
+    return centena_extenso;
+}
+
+const char* escreve_extenso(char *num)
+{
+    static char resultado[1000] = {0};
+    char temp[100];
+    char casas[6][100] = {"quadrilhões","trilhões","bilhões", "milhões", "mil", ""};
+    char casas_unitarias[6][100] = {"quadrilhão", "trilhão", "bilhão", "milhão", "mil", ""};
+    int casa_a_mais = 1;
+
+    memset(resultado, 0, sizeof(resultado));
+
+
+    int j = 0;
+    for(int i = 0; i < (strlen(num) - 3) / 3; i++)
+    {
+        strcpy(temp, separastring(num, j, j+2));
+        j+=3;
+
+        strcpy(temp, centena(temp));
+
+        if(strcmp(temp, "um") == 0)
+        {
+            strcat(resultado, temp);
+            strcat(resultado, " ");
+            strcat(resultado, casas_unitarias[i]);
+            if(i < (strlen(num) - 3) / 3 - 1)
+            {
+                strcat(resultado, " ");
+            }
+            casa_a_mais = 0;
+
+        }
+
+
+        else if(strcmp(temp, "l") != 0)
+        {
+            strcat(resultado, temp);
+            strcat(resultado, " ");
+            strcat(resultado, casas[i]);
+            if(i < (strlen(num) - 3) / 3 - 1)
+            {
+                strcat(resultado, " ");
+            }
+            casa_a_mais = 1;
+
+        }
+
+        strcpy(temp, "");
+
+
+    }
+
+    if(atoi(num) == 1)
+    {
+        strcat(resultado, "real");
+    }
+    else if(casa_a_mais)
+    {
+        strcat(resultado, "reais");
+    }
+    else
+    {
+        strcat(resultado, "de reais");
+    }
+
+
+    strcpy(temp, separastring(num, strlen(num) - 2,strlen(num) - 1));
+
+
+
+    if(strcmp(temp, "00") != 0)
+    {
+
+        strcat(resultado, " e ");
+
+
+        char centavos[100] = "0";
+        strcat(centavos, temp);
+
+        strcat(resultado, centena(centavos));
+
+        strcat(resultado, " centavos.");
+
+    }
+
+
+
+    return resultado;
+    
+}
+
---
+++ main.c
@@ -0,0 +1,249 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <ctype.h>
+#include <math.h>
+#include <unistd.h> 
+#include "funcoes.c"
+
+#define usuario "bancobom"
+#define senha "senhabanco"
+
+typedef struct
+{
+    int dia;
+    int mes;
+    int ano;
+}data;
+
+void limpabuffer()
+{
+    while (getchar() != '\n');
+}
+
+void menu2();
+int menu3();
+
+int main()
+{
+    char user_inserido[100];
+    char senha_inserida[100];
+    int escolha;
+    int ret;
+
+    printf("┌───────────────────────────────────────┐\n");
+    printf("│                                       │\n");
+    printf("│   Bem vindo(a)!                       │\n");
+    printf("│   Insira sua senha para continuar:    │\n");
+    printf("│                                       │\n");
+    printf("└───────────────────────────────────────┘\n");
+
+    printf("Usuário: ");
+    fgets(user_inserido, 100, stdin);
+
+    printf("Senha: ");
+    fgets(senha_inserida, 100, stdin);
+
+    user_inserido[strlen(user_inserido) - 1] = '\0'; // Remove o \n no final da string
+    senha_inserida[strlen(senha_inserida) - 1] = '\0';
+
+    if (strcmp(user_inserido, usuario) != 0 || strcmp(senha_inserida, senha) != 0)
+    {
+        system("clear");
+        printf("┌───────────────────────────────────┐\n");
+        printf("│                                   │\n");
+        printf("│   Usuário ou senha inválidos.     │\n");
+        printf("│   Gostaria de tentar novamente?   │\n");
+        printf("│                                   │\n");
+        printf("│   1-Sim                           │\n");
+        printf("│   2-Não                           │\n");
+        printf("│                                   │\n");
+        printf("└───────────────────────────────────┘\n\n");
+
+        do
+        {
+            printf("Insira sua escolha: ");
+            ret = scanf("%d", &escolha);
+            if (escolha > 2 || escolha < 1 || ret != 1)
+            {
+                printf("\nEscolha inválida.\n");
+                limpabuffer();
+            }
+        } while (ret != 1 || escolha > 2 || escolha < 1);
+
+        if (escolha == 1)
+        {
+            system("clear");
+            limpabuffer();
+            main();
+            return 0;
+        }
+        else
+        {
+            return 0;
+        }
+    }
+
+    menu2();
+}
+
+void menu2()
+{
+    int escolha, ret;
+
+    system("clear");
+    printf("┌───────────────────────────────────┐\n");
+    printf("│                                   │\n");
+    printf("│   Opções:                         │\n");
+    printf("│                                   │\n");
+    printf("│   1-Enviar cheque                 │\n");
+    printf("│   2-Sair                          │\n");
+    printf("│                                   │\n");
+    printf("└───────────────────────────────────┘\n\n");
+
+    do
+    {
+        printf("Insira sua escolha: ");
+        ret = scanf("%d", &escolha);
+        if (escolha > 2 || escolha < 1 || ret != 1)
+        {
+            printf("\nEscolha inválida.\n");
+            limpabuffer();
+        }
+    } while (ret != 1 || escolha > 2 || escolha < 1);
+
+    if (escolha == 1)
+    {
+        printf("\n\n");
+        limpabuffer();
+        system("clear");
+        menu3();
+    }
+    else
+    {
+        system("clear");
+        limpabuffer();
+        main();
+    }
+}
+
+int menu3()
+{
+    int escolha, ret;
+    long double valor;
+    char valor_str[100] = "";
+    char destinatario[100];
+    char nome[100];
+    char espacos[100] = "";
+    char extenso[1000] = "";
+    data data_hoje;
+    int dia, mes, ano;
+
+    do
+    {
+        printf("Insira o valor do cheque: ");
+        ret = scanf("%Lf", &valor);
+
+        if(ret != 1 || valor <= 0 || valor > 999999999999999999.99)
+        {
+            printf("\nEscolha inválida.\n");
+            printf("Tente novamente.\n\n");
+            limpabuffer();
+        }
+    }while(ret != 1 || valor <= 0 || valor > 999999999999999999.99);
+
+    sprintf(valor_str, "%021.2Lf", valor);
+
+    strcpy(extenso, "");
+    
+    strcpy(extenso, escreve_extenso(valor_str)); 
+
+
+
+    limpabuffer();
+
+    printf("Insira o nome do destinatário: ");
+    fgets(destinatario, 100, stdin);
+
+    destinatario[strlen(destinatario) - 1] = '\0';
+
+    printf("Insira a data de hoje(dd/mm/ano) : ");
+    
+    do
+    {
+        scanf("%d/%d/%d", &data_hoje.dia, &data_hoje.mes, &data_hoje.ano);
+        if(data_hoje.dia < 1 || data_hoje.dia > 31 || data_hoje.mes < 1 || data_hoje.mes > 12 || data_hoje.ano < 1)
+        {
+            printf("\nData inválida.\n");
+            printf("Insira a data novamente: ");
+            limpabuffer();
+        }
+    }while(data_hoje.dia < 1 || data_hoje.dia > 31 || data_hoje.mes < 1 || data_hoje.mes > 12 || data_hoje.ano < 1);
+
+
+    
+
+    limpabuffer();
+
+    printf("Insira seu nome: ");
+    fgets(nome, 100, stdin);
+
+    nome[strlen(nome) - 1] = '\0';
+    destinatario[0] = toupper(destinatario[0]);
+    nome[0] = toupper(nome[0]);
+
+    system("clear");
+    
+
+    printf("┏%-128s┓\n", espacos);
+    printf("    Pague por meio\n");
+    printf("    deste cheque uma quantia de %s\n", extenso);
+    printf("    a %s\n", destinatario);
+    printf("    BANCO BOM\n");
+    printf("%-70s%s\n", espacos, nome);
+    printf("%-70s%02d/%02d/%04d\n", espacos, data_hoje.dia, data_hoje.mes, data_hoje.ano);
+    printf("┗%-128s┛\n", espacos);
+
+    sleep(10);
+    system("clear");
+
+
+    printf("┌───────────────────────────────────────┐\n");
+    printf("│                                       │\n");
+    printf("│   Gostaria de enviar outro cheque?    │\n");
+    printf("│                                       │\n");
+    printf("│   1-Sim                               │\n");
+    printf("│   2-Não                               │\n");
+    printf("│                                       │\n");
+    printf("└───────────────────────────────────────┘\n\n");
+
+
+    printf("Insira sua escolha: ");
+    do
+    {
+        ret = scanf("%d", &escolha);
+        if(ret != 1 || escolha > 2 || escolha < 1)
+        {
+            printf("\nEscolha inválida.\n");
+            printf("Tente novamente: ");
+            limpabuffer();
+        }
+    }while(ret != 1 || escolha > 2 || escolha < 1);
+
+    if(escolha == 1)
+    {
+        system("clear");
+        menu3();
+        return 0;
+    }
+    else
+    {
+        return 0;
+    }
+
+
+
+}
+
+
+ 
